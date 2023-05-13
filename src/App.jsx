5f5208b4fa23ac85128ef3f7914ff8d6a54ee7dd
rizkyalfiansyah <rizkkyaf@gmail.com>
2023-05-14 06:25:18 +0800

    fix module responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,20 @@ import React, { useRef, useState } from 'react';
 import PdfFile from './assets/files.pdf';
 import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
 import Dropdown from './components/Dropdown';
+import LogoPNG from './assets/images/logo.png';
 
 export default function App() {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
-  const [collapsed, setCollapsed] = useState(false);
+  const [collapsed, setCollapsed] = useState({
+    profil: false,
+    panduan: false,
+    pengantar: false,
+    topik: false,
+    isi: false,
+    pustaka: false,
+    penutup: false,
+  });
   const docsRef = useRef(null);
 
   function onDocumentLoadSuccess({ numPages }) {
@@ -14,73 +23,134 @@ export default function App() {
   }
 
   return (
-    <section className="w-full min-h-screen bg-hero-pattern backdrop-blur-lg bg-black bg-opacity-50">
-      <div className="w-full h-full backdrop-blur-sm bg-black bg-opacity-30">
-        <div className="relative w-full max-w-fit m-auto min-h-[100dvh] flex flex-col justify-start items-center py-4 gap-4">
-          <Dropdown
-            title="Pilih Topik"
-            onClick={() => setCollapsed(!collapsed)}
-            collapsed={collapsed}
-          >
-            {Array(6)
-              .fill(0)
-              .map((_, i) => (
-                <p
-                  className={`${
-                    collapsed ? 'w-full' : 'hidden'
-                  } p-2 hover:bg-slate-200 cursor-pointer rounded-md`}
-                  onClick={() => {
-                    setPageNumber(2);
-                    docsRef.current.scrollIntoView({ behavior: 'smooth' });
-                    setCollapsed(false);
-                  }}
-                  key={i}
-                >
-                  Topic {i + 1}
-                </p>
-              ))}
-          </Dropdown>
-          <p className="text-white">
-            Page {pageNumber} of {numPages}
-          </p>
-          <div className="w-full h-full rounded-lg">
-            <Document
-              file={PdfFile}
-              onLoadSuccess={onDocumentLoadSuccess}
-              className="transition-all ease-in-out duration-200"
+    <section className="w-full h-[100dvh] bg-hero-pattern backdrop-blur-lg bg-black bg-opacity-50">
+      <div className="w-full h-full backdrop-blur-sm bg-black bg-opacity-30 overflow-auto">
+        <div className="relative max-w-3xl m-auto h-fit flex flex-col justify-start items-center p-4 gap-4">
+          <div className="w-full p-4">
+            <img src={LogoPNG} alt="logo-unm" className="w-full" />
+          </div>
+          <div className="w-full flex flex-col justify-start items-center gap-8">
+            <Dropdown
+              title="Profil"
+              onClick={() =>
+                setCollapsed({ ...collapsed, profil: !collapsed.profil })
+              }
+              collapsed={collapsed.profil}
             >
-              <div className="relative w-full flex h-fit">
-                <div className="w-full absolute top-0 bg-black bg-opacity-30 flex justify-between px-4 py-2 z-[99]">
-                  <button
-                    disabled={pageNumber === 1}
-                    className="px-8 py-2 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed"
-                    onClick={() => setPageNumber(pageNumber - 1)}
-                  >
-                    Prev
-                  </button>
-                  <button
-                    disabled={pageNumber === numPages}
-                    className="px-8 py-2 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed"
+              <h1>Profil</h1>
+            </Dropdown>
+            <Dropdown
+              title="Panduan Penggunaan Modul"
+              onClick={() =>
+                setCollapsed({ ...collapsed, panduan: !collapsed.panduan })
+              }
+              collapsed={collapsed.panduan}
+            >
+              <h1>Panduan Penggunaan Modul</h1>
+            </Dropdown>
+            <Dropdown
+              title="Pengantar"
+              onClick={() =>
+                setCollapsed({ ...collapsed, pengantar: !collapsed.pengantar })
+              }
+              collapsed={collapsed.pengantar}
+            >
+              <h1>Pengantar</h1>
+            </Dropdown>
+            <Dropdown
+              title="Modul"
+              onClick={() =>
+                setCollapsed({ ...collapsed, topik: !collapsed.topik })
+              }
+              collapsed={collapsed.topik}
+            >
+              {/* {Array(6)
+                .fill(0)
+                .map((_, i) => (
+                  <p
+                    className={`w-full p-2 hover:bg-slate-200 cursor-pointer rounded-md`}
                     onClick={() => {
-                      setPageNumber(pageNumber + 1);
+                      setPageNumber(2);
+                      docsRef.current.scrollIntoView({ behavior: 'smooth' });
+                      setCollapsed({ ...collapsed, topik: false });
                     }}
+                    key={i}
                   >
-                    Next
-                  </button>
+                    Topik {i + 1}
+                  </p>
+                ))} */}
+              <p
+                className={`w-full p-2 hover:bg-slate-200 cursor-pointer rounded-md`}
+                onClick={() => {
+                  setPageNumber(2);
+                  docsRef.current.scrollIntoView({ behavior: 'smooth' });
+                  setCollapsed({ ...collapsed, topik: false });
+                }}
+              >
+                Topik 3
+              </p>
+            </Dropdown>
+            <Dropdown
+              title="Daftar Isi"
+              onClick={() =>
+                setCollapsed({ ...collapsed, isi: !collapsed.isi })
+              }
+              collapsed={collapsed.isi}
+            >
+              <h1>Daftar Isi</h1>
+            </Dropdown>
+            <Dropdown
+              title="Daftar Pustaka"
+              onClick={() =>
+                setCollapsed({ ...collapsed, pustaka: !collapsed.pustaka })
+              }
+              collapsed={collapsed.pustaka}
+            >
+              <h1>Daftar Pustaka</h1>
+            </Dropdown>
+            <Dropdown
+              title="Penutup"
+              onClick={() =>
+                setCollapsed({ ...collapsed, penutup: !collapsed.penutup })
+              }
+              collapsed={collapsed.penutup}
+            >
+              <h1>Penutup</h1>
+            </Dropdown>
+            <div className="w-full h-full rounded-lg flex flex-col gap-2">
+              <Document file={PdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+                <div className="relative w-full flex h-fit border border-primary-100">
+                  <div className="w-full absolute top-0 bg-black bg-opacity-30 flex justify-between px-4 py-2 z-[99]">
+                    <div className="flex items-center gap-2">
+                      <button
+                        disabled={pageNumber === 1}
+                        className="px-4 py-1 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed text-xs"
+                        onClick={() => setPageNumber(pageNumber - 1)}
+                      >
+                        Prev
+                      </button>
+                      <div className="bg-white px-2 py-1 rounded-sm">
+                        <p className="text-center text-xs">
+                          Page {pageNumber} of {numPages}
+                        </p>
+                      </div>
+                    </div>
+                    <button
+                      disabled={pageNumber === numPages}
+                      className="px-4 py-1 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed text-xs"
+                      onClick={() => {
+                        setPageNumber(pageNumber + 1);
+                      }}
+                    >
+                      Next
+                    </button>
+                  </div>
+                  <div className="w-full" ref={docsRef}>
+                    <Page pageNumber={pageNumber} renderTextLayer={false} />
+                  </div>
                 </div>
-                <div className="w-full transition-all" ref={docsRef}>
-                  <Page
-                    width={
-                      window.innerWidth <= 768
-                        ? window.innerWidth * 0.95
-                        : window.innerWidth * 0.5
-                    }
-                    pageNumber={pageNumber}
-                    renderTextLayer={false}
-                  />
-                </div>
-              </div>
-            </Document>
+              </Document>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -4,18 +4,22 @@ import { ReactComponent as ChevronSvg } from '../assets/icons/chevron.svg';
 const Dropdown = ({ title, onClick, collapsed, children }) => {
   return (
     <div className="relative w-full flex flex-col gap-2">
-      <div
-        className="w-full p-3 rounded-full bg-primary-100 text-blue-950 text-center hover:bg-primary-200 cursor-pointer border-2 border-black"
-        onClick={onClick}
-      >
-        <h3 className="font-semibold text-xl">{title}</h3>
+      <div className="relative w-full h-14 rounded-full bg-[#C7B5AA] hover:bg-transparent">
+        <div
+          className="absolute -top-3 w-full p-3 rounded-full bg-primary-200 text-blue-950 text-center hover:top-0 cursor-pointer border border-black"
+          onClick={onClick}
+        >
+          <h3 className="font-semibold text-xl">{title}</h3>
+        </div>
       </div>
       <div
         className={`${
           collapsed ? 'min-h-[100px] bg-white shadow-lg' : 'min-h-0 shadow-none'
         } w-full rounded-md shadow-lg transition-all ease-in-out duration-500`}
       >
-        <div className="w-full p-2">{children}</div>
+        <div className={`${collapsed ? 'block w-full p-2' : 'w-0 hidden p-0'}`}>
+          {children}
+        </div>
       </div>
     </div>
   );
