31eec476eaeba92633e7d58698c6d4567d7f8eac
rizkyalfiansyah <rizkkyaf@gmail.com>
2023-05-14 01:13:22 +0800

    fix : layout

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <section className="w-full min-h-screen bg-gray-200">
       <div className="relative w-full max-w-fit m-auto min-h-[100dvh] flex flex-col justify-start items-center py-4 gap-4">
-        <div className="w-full flex flex-col gap-2">
+        <div className="relative w-full flex flex-col gap-2">
           <div
             className="w-full p-3 rounded-md bg-orange-500 text-white hover:bg-orange-400 cursor-pointer"
             onClick={() => setCollapsed(!collapsed)}
@@ -25,13 +25,14 @@ export default function App() {
           <div
             className={`${
               collapsed ? 'w-full' : 'w-0 hidden'
-            } bg-white min-h-fit p-3 rounded-md shadow-lg transition-all`}
+            } bg-white min-h-fit p-3 rounded-md shadow-lg transition-all absolute top-14 left-0 z-100`}
           >
             <p
               className="w-full p-2 hover:bg-slate-200 cursor-pointer rounded-md"
               onClick={() => {
                 setPageNumber(2);
                 docsRef.current.scrollIntoView({ behavior: 'smooth' });
+                setCollapsed(false);
               }}
             >
               Topic 3 : Otonomi
@@ -47,7 +48,7 @@ export default function App() {
               className="relative flex h-fit flex-col justify-center items-center"
               ref={docsRef}
             >
-              <div className="w-full absolute top-0 bg-black bg-opacity-30 flex justify-between px-4 py-2 z-100">
+              <div className="w-full absolute top-0 bg-black bg-opacity-30 flex justify-between px-4 py-2 z-[99]">
                 <button
                   disabled={pageNumber === 1}
                   className="px-8 py-2 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed"
@@ -66,7 +67,7 @@ export default function App() {
                 </button>
               </div>
               <Page
-                width={window.innerWidth <= 768 ? 320 : 800}
+                width={window.innerWidth <= 768 ? 350 : 800}
                 // height={window.innerWidth <= 768 ? 300 : 400}
                 pageNumber={pageNumber}
                 renderTextLayer={false}
