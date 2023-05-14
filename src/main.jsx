5a35f686af5b8b3b6c39d000b4e7e4bd0f373947
rizkyalfiansyah <rizkkyaf@gmail.com>
2023-05-14 20:57:30 +0800

    uncomment

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from './components/pages/Dashboard';
 import Modules from './components/pages/Modules';
-import Demo from './components/pages/Demo';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Demo /> },
+  { path: '/', element: <Dashboard /> },
   {
     path: '/moduls',
     element: <Modules />,
