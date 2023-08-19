9b3c0a553c121749a69c2d35d7e1d64aaaa3cff7
rizkyalfiansyah <rizkkyaf@gmail.com>
2023-08-20 01:16:50 +0800

    feat : add logo and loading

--- src/components/pages/Modules.jsx
+++ src/components/pages/Modules.jsx
@@ -5,10 +5,9 @@ import Topik3PDF from '../../assets/e-modul/topik-3-isi.pdf';
 import Topik4PDF from '../../assets/e-modul/topik-4-isi.pdf';
 import Topik5PDF from '../../assets/e-modul/topik-5-isi.pdf';
 import Topik6PDF from '../../assets/e-modul/topik-6-isi.pdf';
-import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
+import PdfFileRender from '../organism/PdfFileRender';
 
 const PdfFile = [
-  '',
   Topik1PDF,
   Topik2PDF,
   Topik3PDF,
@@ -34,45 +33,7 @@ export default function Modules() {
       <div className="w-full h-full backdrop-blur-sm bg-black bg-opacity-30 overflow-hidden">
         <div className="relative max-w-3xl m-auto h-fit flex flex-col justify-start items-center p-4 gap-4">
           <div className="w-full h-full rounded-lg flex flex-col gap-2">
-            <Document
-              file={PdfFile[params.get('topik')]}
-              onLoadSuccess={onDocumentLoadSuccess}
-              className="w-full h-full"
-            >
-              <div className="relative w-full flex h-fit border border-primary-100">
-                <div className="w-full absolute top-0 bg-black bg-opacity-30 flex justify-between px-4 py-2 z-[99]">
-                  <div className="flex items-center gap-2">
-                    <button
-                      disabled={pageNumber === 1}
-                      className="px-4 py-1 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed text-xs"
-                      onClick={() => setPageNumber(pageNumber - 1)}
-                    >
-                      Prev
-                    </button>
-                    <div className="bg-white px-2 py-1 rounded-sm">
-                      <p className="text-center text-xs">
-                        Page {pageNumber} of {numPages}
-                      </p>
-                    </div>
-                  </div>
-                  <button
-                    disabled={pageNumber === numPages}
-                    className="px-4 py-1 bg-blue-500 rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed text-xs"
-                    onClick={() => {
-                      setPageNumber(pageNumber + 1);
-                    }}
-                  >
-                    Next
-                  </button>
-                </div>
-                <div
-                  className="w-full transition-all ease-in-out duration-150"
-                  ref={docsRef}
-                >
-                  <Page pageNumber={pageNumber} renderTextLayer={false} />
-                </div>
-              </div>
-            </Document>
+            <PdfFileRender PdfFile={PdfFile[pageNumber - 1]} />
           </div>
         </div>
       </div>
